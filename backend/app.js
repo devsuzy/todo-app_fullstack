d3d4dcbfbd824158cb0a948edc10964aa3894527
devsuzy <suzy0529@gmail.com>
2026-05-15 16:54:34 +0900

    fix: import cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import Task from './models/Task.js';
 import mongoose from "mongoose";
+import cors from 'cors';
 
 const app = express();
 
